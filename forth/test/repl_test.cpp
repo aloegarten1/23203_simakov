a4782483f550d0460e3d1209c006702a2e2014a1
aloegarten1 <s.simakov@g.nsu.ru>
2025-01-05 19:32:45 +0700

    tests: PLUS

--- forth/test/repl_test.cpp
+++ forth/test/repl_test.cpp
@@ -4,26 +4,51 @@
 
 using namespace std;
 
-
-Repl MockRepl(std::string input){
-
+Repl MockRepl(std::string input, std::ostream &os)
+{
 
     std::istringstream is(input);
-    return Repl(is);
+    return Repl(is, os);
 }
 
-TEST(replt, test1)
+TEST(REPL, PushOnePositiveNumber)
 {
-}
+    std::ostringstream os;
 
-TEST(NumberCheck, PushOnePositiveNumber)
-{
-    Repl repl = MockRepl("1");
+    Repl repl = MockRepl("123\n.\n", os);
     repl.run();
-    // Interpreter &i = Interpreter::getInstance();
-    // std::string input = "1";
-    // EXPECT_EQ(i.interpret(input), "< ok\n");
 
-    // input = "drop 12345 .";
-    // EXPECT_EQ(i.interpret(input), "12345\n");
+    EXPECT_EQ(os.str(), "123 ");
 }
+
+
+TEST(REPL, PLUS_BAD)
+{
+    std::ostringstream os;
+
+    Repl repl = MockRepl("2\n2\n+\n.\n", os);
+    repl.run();
+
+    EXPECT_NE(os.str(), "44");
+}
+
+TEST(REPL, PLUS_BAD_ES)
+{
+    std::ostringstream os;
+
+    Repl repl = MockRepl("55\n+\n", os);
+    repl.run();
+
+    EXPECT_EQ(os.str(), "exec error: +\n");
+}
+
+TEST(REPL, PLUS_GOOD)
+{
+    std::ostringstream os;
+
+    Repl repl = MockRepl("2\n2\n+\n.\n", os);
+    repl.run();
+
+    EXPECT_EQ(os.str(), "4 ");
+}
+
