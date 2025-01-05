928938d63157c687a3b6ed2080cb2d4fe316b814
aloegarten1 <s.simakov@g.nsu.ru>
2025-01-05 16:15:34 +0700

    cleanup

--- forth/CMakeFiles/forth.dir/interpreter/basics/expression.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/forth.dir/interpreter/basics/expression.cpp.o: \
- /home/stepan/projects/cpp/23203_simakov/forth/interpreter/basics/expression.cpp \
- /usr/include/stdc-predef.h
--- forth/CMakeFiles/forth.dir/interpreter/internals/executionContext.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/forth.dir/interpreter/internals/executionContext.cpp.o: \
- /home/stepan/projects/cpp/23203_simakov/forth/interpreter/internals/executionContext.cpp \
- /usr/include/stdc-predef.h
